806933b36686262d0e4092e514c902d2a199f781
mechAneko <naokih1998@yahoo.co.jp>
2019-10-17 23:36:22 +0900

    Study app

--- manage.py
+++ manage.py
@@ -5,7 +5,6 @@ import sys
 
 
 def main():
-    sys.setrecursionlimit(10000)
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'manager_project.settings')
     try:
         from django.core.management import execute_from_command_line
