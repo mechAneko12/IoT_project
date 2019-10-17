cb91e8eed13ae01c13f57dc0138bfc40a8546d65
mechAneko <naokih1998@yahoo.co.jp>
2019-10-18 00:36:55 +0900

    Study app

--- manager_project/wsgi.py
+++ manager_project/wsgi.py
@@ -18,5 +18,5 @@ application = get_wsgi_application()
 
 
 
-#from whitenoise.django import DjangoWhiteNoise
-#application = DjangoWhiteNoise(application)
+from whitenoise.django import DjangoWhiteNoise
+application = DjangoWhiteNoise(application)
