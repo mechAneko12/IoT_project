fa92c0a4d6c47be0348a8d7b8e6985b117fb010a
mechAneko <naokih1998@yahoo.co.jp>
2019-10-18 01:38:43 +0900

    Study  app

--- manager_project/urls.py
+++ manager_project/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('manager_project/',include("manager_project.urls")),
+    path('manager/',include('manager_project.urls')),
 ]
