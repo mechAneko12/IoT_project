248284b1449bf7534260352cbef446bb5b8f8ecd
mechAneko <naokih1998@yahoo.co.jp>
2019-10-19 23:02:56 +0900

    Study app

--- manager/urls.py
+++ manager/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
   path("",views.index,name="index"),
-  path("",views.mainjs,name="main.js"),
-  path("",views.stylecss,name="style.css"),
+  #path("",views.mainjs,name="main.js"),
+  #path("",views.stylecss,name="style.css"),
 ]
