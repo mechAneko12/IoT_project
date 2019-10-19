3ba94bf0bb7f6d0689f68f037301fa152561800c
mechAneko <naokih1998@yahoo.co.jp>
2019-10-19 22:36:15 +0900

    Study app

--- manager/views.py
+++ manager/views.py
@@ -4,3 +4,11 @@ from django.shortcuts import render
 def index(request):
   template_name = "index.html" # templates以下のパスを書く
   return render(request,template_name)
+
+def mainjs(request):
+  template_name = "main.js" # templates以下のパスを書く
+  return render(request,template_name)
+
+def stylecss(request):
+  template_name = "style.css" # templates以下のパスを書く
+  return render(request,template_name)
