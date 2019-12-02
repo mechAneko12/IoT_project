0e9e68cad705191f3a04eedd6c3d2de0248724ce
mechAneko <naokih1998@yahoo.co.jp>
2019-12-02 20:04:39 +0900

    Study app

--- manager_project/urls.py
+++ manager_project/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from manager import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('manager.urls')),
