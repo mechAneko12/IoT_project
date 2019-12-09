920b09d4d0e7ce2ebd7e54c3600e44e18cadd647
mechAneko <naokih1998@yahoo.co.jp>
2019-12-09 21:46:49 +0900

    Study app

--- manager/views.py
+++ manager/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import JsonResponse
+from django.http.response import JsonResponse
 from .models import Data
 import datetime
 from django.core import serializers
