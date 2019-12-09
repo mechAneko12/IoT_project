3c7a15a4717994e7edda7f160a6bdd19640b9bfe
mechAneko <naokih1998@yahoo.co.jp>
2019-12-09 20:10:22 +0900

    Study app

--- manager_project/urls.py
+++ manager_project/urls.py
@@ -22,5 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('manager.urls')),
     path('table/', views.table, name='table'),
-    path("sql_post/", views.sql_post, name = "sql_post"),
+    #path("sql_post/", views.sql_post, name = "sql_post"),
 ]
