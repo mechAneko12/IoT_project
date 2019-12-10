96b61e80e72ae44b7e2c720e9cd54fe01c224414
mechAneko <naokih1998@yahoo.co.jp>
2019-12-11 02:56:17 +0900

    Study app

--- manager/views.py
+++ manager/views.py
@@ -141,7 +141,7 @@ def sql_update(request):
                               work_time = work_time,
                               actual_work_time = actual_work_time,
                               efficiency =efficiency)
-  d ={"id": data.id}
+  d ={"id": Id}
   return JsonResponse(d)
 
   '''
