b787c50e1db8125995cc5ec6d074061dc9966683
mechAneko <naokih1998@yahoo.co.jp>
2019-12-12 00:49:24 +0900

    Study app

--- manager/urls.py
+++ manager/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
   #path("",views.stylecss,name="style.css"),
   path("sql_post/", views.sql_post, name = "sql_post"),
   path("sql_update/", views.sql_update, name = "sql_update"),
-  path("table/sql_get/", views.sql_get, name = "sql_get"),
+  #path("table/sql_get/", views.sql_get, name = "sql_get"),
 ]
